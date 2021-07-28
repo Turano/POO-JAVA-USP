d649732334eda35af048d057169eb91758e86c69
LuisFelipeJorge <luisfelipe.jorge123@gmail.com>
2021-07-27 22:58:37 -0300

    diminuir textos

--- ProjetoFinal/src/ClassificacaoDePeso.java
+++ ProjetoFinal/src/ClassificacaoDePeso.java
@@ -1,6 +1,5 @@
 
 public enum ClassificacaoDePeso {
-<<<<<<< HEAD
 	ABAIXO_DO_PESO(new String[] { "Abaixo do peso.", "Se manter em um estado de peso muito baixo pode acarretar sintomas como "
 			+ "queda de cabelo, infertilidade, ausencia menstrual, fadiga, stres e ansiedade"}), 
 	NORMAL(new String[] {"Peso adequado.", "Menor risco de doencas cardiacas e vasculares."}), 
@@ -10,27 +9,8 @@ public enum ClassificacaoDePeso {
 	OBESIDADE_GRAU2(new String[] {"Obesidade de grau 2.", "O estado de obesidade esta intimimamente associado como fator de risco para"
 			+ " doencas vasculares e cardiacas alem de outras como apneia do sono, falta de ar."}), 
 	OBESIDADE_GRAU3(new String[] {"Obesidade de grau 3.", "O estado de obesidade esta intimimamente associado como fator de risco para "
-			+ "doencas vasculares e cardiacas como diabetes, infarto, AVC e além delas outras como"
-			+ " refluxo, dificuldade para se mover, escaras."});
+			+ "doencas vasculares e cardiacas como diabetes, infarto, AVC."});
 	
-=======
-	ABAIXO_DO_PESO(new String[] { "Abaixo do peso.",
-			"Se manter em um estado de peso muito baixo pode acarretar sintomas como "
-					+ "queda de cabelo, infertilidade, ausencia menstrual, fadiga, stres e ansiedade" }),
-	NORMAL(new String[] { "Peso adequado.", "Menor risco de doencas cardaacas e vasculares." }),
-	SOBREPESO(new String[] { "Sobrepeso.",
-			"Estar em sobrepeso pode ocasionar sintomas como fadiga, ma circulacao, varizes" }),
-	OBESIDADE_GRAU1(new String[] { "Obesidade de grau 1.",
-			"O estado de obesidade esta intimimamente associado como fator de risco para doencas como"
-					+ " diabetes, angina, infarto, aterosclerose." }),
-	OBESIDADE_GRAU2(new String[] { "Obesidade de grau 2.",
-			"O estado de obesidade esta intimimamente associado como fator de risco para"
-					+ " doencas vasculares e cardiacas alem de outras como apneia do sono, falta de ar." }),
-	OBESIDADE_GRAU3(new String[] { "Obesidade de grau 3.",
-			"O estado de obesidade esta intimimamente associado como fator de risco para "
-					+ "doencas vasculares e cardiacas como diabetes, infarto, AVC, etc." });
-
->>>>>>> 6fb15f894c879dc752cd15dc72f7bef40fad7f8e
 	private String[] classificacao;
 
 	ClassificacaoDePeso(String[] classificacao) {
